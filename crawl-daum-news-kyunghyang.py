ade1ca9d7099429a000744b37307bbbd1dd3b52c
cookieshake <cookieshake.dev@gmail.com>
2019-01-01 04:01:26 +0900

    add dag crawl-daum-news-kyunghyang

---
+++ crawl-daum-news-kyunghyang.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+import pendulum
+
+from datetime import datetime, timedelta
+
+from util import crawl, temp_json_to_parquet
+
+local_tz = pendulum.timezone('Asia/Seoul')
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2018, 1, 1, tzinfo=local_tz),
+    'email': ['cookieshake.dev@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=30),
+    'schedule_interval': '0 2 * * *'
+}
+
+dag = DAG('crawl-daum-news-kyunghyang', default_args=default_args)
+
+task_crawl = PythonOperator(
+    task_id='task_crawl',
+    provide_context=True,
+    python_callable=crawl,
+    op_kwargs={'media_code': 11},
+    dag=dag)
+
+task_temp_json_to_parquet = PythonOperator(
+    task_id='task_temp_json_to_parquet',
+    provide_context=True,
+    python_callable=temp_json_to_parquet,
+    op_kwargs={'media_code': 11},
+    dag=dag)
+
+task_crawl >> task_temp_json_to_parquet
---
+++ util.py
@@ -0,0 +1,71 @@
+from scrapy.crawler import CrawlerProcess
+from spiders.daum_news import DaumNewsSpider
+
+import pandas as pd
+
+from airflow.hooks.base_hook import BaseHook
+from datetime import datetime, timedelta
+from tempfile import TemporaryDirectory
+import os
+from pathlib import Path
+import uuid
+
+import boto3
+
+def crawl(media_code, **kwargs):
+    execution_date = kwargs['execution_date']
+    temp_dir = TemporaryDirectory()
+    temp_file_name = str(uuid.uuid4()) + '.json'
+    temp_file = Path(temp_dir.name) / temp_file_name
+
+    crawler_process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:64.0) Gecko/20100101 Firefox/64.0',
+        'ROBOTSTXT_OBEY': False,
+        'FEED_FORMAT': 'jsonlines',
+        'FEED_URI': str(temp_file)
+    })
+    
+    crawler = crawler_process.create_crawler(DaumNewsSpider)
+    crawler_process.crawl(crawler, media_code=media_code, reg_date=execution_date.strftime('%Y%m%d'))
+    crawler_process.start(stop_after_crawl=True)
+
+    if 'log_count/ERROR' in crawler.stats.get_stats():
+        raise Exception('Error log exists')
+
+    conn = BaseHook.get_connection('wasabi')
+    s3 = boto3.resource('s3',
+        endpoint_url = conn.extra_dejson['endpoint_url'],
+        aws_access_key_id = conn.extra_dejson['aws_access_key_id'],
+        aws_secret_access_key = conn.extra_dejson['aws_secret_access_key'],
+        verify=False)
+    
+    s3.meta.client.upload_file(str(temp_file), 'ingtranet-temp', temp_file_name)
+    temp_dir.cleanup()
+    return temp_file_name
+
+def temp_json_to_parquet(media_code, **kwargs):
+    execution_date = kwargs['execution_date']
+    temp_file_name = kwargs['task_instance'].xcom_pull(task_ids='task_crawl')
+
+    conn = BaseHook.get_connection('wasabi')
+    
+    s3 = boto3.resource('s3',
+        endpoint_url = conn.extra_dejson['endpoint_url'],
+        aws_access_key_id = conn.extra_dejson['aws_access_key_id'],
+        aws_secret_access_key = conn.extra_dejson['aws_secret_access_key'])
+
+    with TemporaryDirectory() as temp_dir:
+        temp_file = Path(temp_dir) / temp_file_name
+        s3.Bucket('ingtranet-temp').download_file(temp_file_name, str(temp_file))
+
+        dfs = pd.read_json(str(temp_file), lines=True, convert_dates=[''], chunksize=750)
+        
+        for i, df in enumerate(dfs):
+            file_path = Path(temp_dir) / '{}.parquet.gzip'.format(i)
+            df.to_parquet(str(file_path), compression='gzip')
+  
+            s3.meta.client.upload_file(str(file_path), 'ingtranet-library', 'daum_news/{}/{}/{}/{}/{}'.format(
+                media_code, execution_date.year, execution_date.month, execution_date.day, file_path.name
+            ))
+    
+        
