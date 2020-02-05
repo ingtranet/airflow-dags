e9e6be3e10bd5e818d3aa03a63b674f5a446fea9
cookieshake <cookieshake.dev@gmail.com>
2020-02-05 23:25:34 +0900

    change all to docker

--- dags/crawl-daum-news-kyunghyang.py
+++ dags/crawl-daum-news-kyunghyang.py
@@ -1,11 +1,10 @@
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
-
-import pendulum
-
+import textwrap
 from datetime import datetime, timedelta
 
-from util import crawl, temp_json_to_parquet, branch
+from airflow import DAG
+from airflow.operators.docker_operator import DockerOperator
+
+import pendulum
 
 local_tz = pendulum.timezone('Asia/Seoul')
 default_args = {
@@ -15,33 +14,27 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 3,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=3)
 }
 
-dag = DAG('crawl-daum-news-kyunghyang', 
+dag = DAG('crawl-daum-news-chosun', 
     default_args=default_args,
-    concurrency=3, 
-    max_active_runs=3,
-    schedule_interval='0 2 * * *')
+    concurrency=2, 
+    max_active_runs=2,
+    schedule_interval='0 2 * * *'
+)
 
-task_crawl = PythonOperator(
-    task_id='task_crawl',
-    provide_context=True,
-    python_callable=crawl,
-    op_kwargs={'media_code': 11},
-    dag=dag)
-
-task_branch = BranchPythonOperator(
-    task_id='task_branch',
-    provide_context=True,
-    python_callable=branch,
-    dag=dag)
-
-task_temp_json_to_parquet = PythonOperator(
-    task_id='task_temp_json_to_parquet',
-    provide_context=True,
-    python_callable=temp_json_to_parquet,
-    op_kwargs={'media_code': 11},
-    dag=dag)
-
-task_crawl >> task_branch >> task_temp_json_to_parquet
+crawl = DockerOperator(
+    dag=dag,
+    force_pull=True,
+    task_id='crawl',
+    image='ingtranet/crawling',
+    api_version='auto',
+    auto_remove=True,
+    command=textwrap.dedent("""
+        daum_news -a code=11 -a date={{ ds_nodash }}
+    """),
+    environment={
+        'MONGO_HOST': 'mongodb0.mrnet:27017'
+    }
+)
