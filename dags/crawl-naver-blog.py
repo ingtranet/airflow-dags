e707db919ef4ae1551a33d18e7d307999855e704
YoungChan Kim <cookieshake@linecorp.com>
2020-02-06 19:08:07 +0900

    add blog crawler

---
+++ dags/crawl-naver-blog.py
@@ -0,0 +1,41 @@
+import textwrap
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.docker_operator import DockerOperator
+
+import pendulum
+
+local_tz = pendulum.timezone('Asia/Seoul')
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2020, 2, 5, tzinfo=local_tz),
+    'email': ['cookieshake.dev@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=3)
+}
+
+dag = DAG('crawl-naver-blog', 
+    default_args=default_args,
+    concurrency=1, 
+    max_active_runs=1,
+    schedule_interval='0 */6 * * *',
+    catchup=False
+)
+
+crawl = DockerOperator(
+    dag=dag,
+    force_pull=True,
+    task_id='crawl',
+    image='ingtranet/crawling',
+    api_version='auto',
+    auto_remove=True,
+    command=textwrap.dedent("""
+        naver_blog
+    """),
+    environment={
+        'MONGO_HOST': 'mongodb0.mrnet:27017'
+    }
+)
