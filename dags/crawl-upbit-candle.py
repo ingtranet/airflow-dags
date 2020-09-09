571b7d5611bcb06b13961c7300f12ddbe00897a1
cookieshake <cookieshake.dev@gmail.com>
2020-09-09 12:30:49 +0900

    increase concurrency

--- dags/crawl-upbit-candle.py
+++ dags/crawl-upbit-candle.py
@@ -21,7 +21,7 @@ default_args = {
 
 dag = DAG('crawl-upbit-candle', 
     default_args=default_args,
-    concurrency=1, 
+    concurrency=2, 
     max_active_runs=1,
     schedule_interval='* * * * *',
     catchup=False
