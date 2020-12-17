6378e8f5c18aeb6b72f4ad96d0227de5e151b2e9
YoungChan Kim <cookieshake@linecorp.com>
2020-12-17 12:56:32 +0900

    fix typo

--- dags/mule-coupang-tools.py
+++ dags/mule-coupang-tools.py
@@ -102,4 +102,4 @@ update_products = DockerOperator(
     dag=dag,
 )
 
-best_categories >> update_searches >> recommend >> update_click_urls >> update_products
+goldbox >> best_categories >> update_searches >> recommend >> update_click_urls >> update_products
