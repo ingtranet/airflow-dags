ec63f7b404b3d63c03c819b0ba4c77d0bf55a72f
cookieshake <cookieshake.dev@gmail.com>
2022-06-06 21:13:02 +0900

    use pyhive in kyuubi operator

--- tools/operators/kyuubi.py
+++ tools/operators/kyuubi.py
@@ -1,7 +1,16 @@
 from typing import Any
 
-from airflow.providers.apache.hive.operators.hive import HiveOperator
+from airflow.models import BaseOperator
+from airflow.utils.context import Context
 
-class KyuubiOperator(HiveOperator):
-    def __init__(self, sql: str, **kwargs: Any) -> None:
-        super().__init__(hql=sql, **kwargs)
+from airflow.providers.apache.hive.hooks.hive import HiveServer2Hook
+
+class KyuubiOperator(BaseOperator):
+    def __init__(self, sql: str, conn_id='kyuubi-default', **kwargs):
+        self.sql = sql
+        self.conn_id = conn_id
+        super().__init__(**kwargs)
+
+    def execute(self, context: Context) -> Any:
+        result = HiveServer2Hook(hiveserver2_conn_id=self.conn_id).get_results(hql=self.sql)
+        print(f'Query Result: {result}')
